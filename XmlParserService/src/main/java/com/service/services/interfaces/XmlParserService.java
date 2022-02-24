3241d1387719169b568941a426f40cf878521f95
sriramez <velayutham.sriram@gmail.com>
2022-02-24 13:32:40 +0530

    updated application code with subscriber pattern

---
+++ XmlParserService/src/main/java/com/service/services/interfaces/XmlParserService.java
@@ -0,0 +1,27 @@
+package com.service.services.interfaces;
+
+import com.service.exception.XmlAddExployeeException;
+import com.service.exception.XmlReadException;
+import com.service.exception.XmlRemoveEmployeeException;
+import com.service.exception.XmlValidationException;
+import com.service.exception.XmlWriteException;
+
+import xmlparser.XMLValidator;
+
+public interface XmlParserService {
+
+	String readXMLFileToString(String filePath, String key) throws XmlValidationException, XmlReadException;
+
+	String readXMLFileToString(String filePath) throws XmlReadException, XmlValidationException;
+
+	String addEmployees(String key, String contents) throws XmlValidationException, XmlAddExployeeException;
+
+	String removeEmployee(String param, String value, String key) throws XmlRemoveEmployeeException;
+
+	String getOutputFile(String key) throws XmlWriteException;
+
+	String loadValidator(String absolutePath, String key) throws XmlValidationException;
+
+	public XMLValidator getValidator(String key);
+
+}
